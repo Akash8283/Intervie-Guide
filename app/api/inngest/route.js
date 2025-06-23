5046519ac26136a463c4bb5aaa57f8a734b6b318
Akash K S <162964515+Akash8283@users.noreply.github.com>
2025-06-23 14:12:55 +0530

    Update route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -7,7 +7,6 @@ import { serve } from "inngest/next";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [generateIndustryInsights],
-  streaming: "allow",
+  functions: [generateIndustryInsights]
 });
 
